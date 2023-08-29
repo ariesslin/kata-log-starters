b643716ba7d675174666d8d1e7d98b700a81aa4e
Ariess Lin <ariess.lin@gmail.com>
2023-08-29 19:11:55 +0200

    Apply my best practice for the 2d grid setup

--- christmas-lights-test.js
+++ christmas-lights-test.js
@@ -1,7 +1,26 @@
 "use strict";
 
-const Greeter = require("./christmas-lights.js");
+const ChristmasLights = require("./christmas-lights.js");
 
-test("test runner is set up correctly", () => {
+it("should initialize lightGrid with given height and width", () => {
+    const lightGrid = new ChristmasLights(5, 5);
+
+    expect(lightGrid.width).toBe(5);
+    expect(lightGrid.height).toBe(5);
+});
+
+it("should get and set cell values", () => {
+    const lightGrid = new ChristmasLights(5, 5);
+
+    lightGrid.setCell(2, 2, 1);
+    expect(lightGrid.getCell(2, 2)).toBe(1);
+});
+
+it("turn on 0,0 through 999,999 would turn on (or leave on) every light", () => {
+    // Arrange
+
+    // Act
+
+    // Assert
     expect(1).toBe(1);
 });
--- christmas-lights.js
+++ christmas-lights.js
@@ -1,5 +1,37 @@
 "use strict";
 
-class ChristmasLights {}
+class ChristmasLights {
+    #width;
+    #height;
+    #grid;
+
+    constructor(width, height, defaultFilling = 0) {
+        this.#width = width;
+        this.#height = height;
+        this.#grid = this.#initializeGrid(defaultFilling);
+    }
+
+    #initializeGrid(defaultFilling) {
+        return new Array(this.#height)
+            .fill(null)
+            .map(() => new Array(this.#width).fill(defaultFilling));
+    }
+
+    get width() {
+        return this.#width;
+    }
+
+    get height() {
+        return this.#height;
+    }
+
+    getCell(i, j) {
+        return this.#grid[i][j];
+    }
+
+    setCell(i, j, value) {
+        this.#grid[i][j] = value;
+    }
+}
 
 module.exports = ChristmasLights;
