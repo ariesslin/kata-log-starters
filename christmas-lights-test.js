ea9b4b5001166d30bbdccf5532c9eacbdce90990
Ariess Lin <ariess.lin@gmail.com>
2023-08-29 18:25:01 +0200

    Initial commit

---
+++ christmas-lights-test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const Greeter = require("./christmas-lights.js");
+
+test("test runner is set up correctly", () => {
+    expect(1).toBe(1);
+});
---
+++ christmas-lights.js
@@ -0,0 +1,5 @@
+"use strict";
+
+class ChristmasLights {}
+
+module.exports = ChristmasLights;
